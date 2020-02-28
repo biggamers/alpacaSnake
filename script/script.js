71a838e169570fad086b025e5f56c280ca0d906d
biggamers <61527766+biggamers@users.noreply.github.com>
2020-02-28 07:13:42 +0200

    v1.0

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 // Задаем поле игры
-let fieldHeight = 15, fieldWidth = 21, count = 1, seconds = 0, minutes = 0, time = '';
+let fieldHeight = 13, fieldWidth = 21, count = 1, seconds = 0, minutes = 0, time = '';
 let field = document.createElement('div');
 document.body.appendChild(field);
 field.classList.add('field');
